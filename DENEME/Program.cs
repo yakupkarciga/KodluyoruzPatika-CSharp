d45d7bbc5a99c59714157a55bface27f2442dee0
Batuhan Kıltaç <81926452+lbakteri@users.noreply.github.com>
2021-08-12 15:58:24 +0300

    Add files via upload

---
+++ DENEME/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DENEME
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            Console.Write("Adınızı giriniz: ");
+            string name = Console.ReadLine();
+            Console.Write("Soyadınızı giriniz: ");
+            string surname = Console.ReadLine();
+
+            Console.WriteLine("Hoşgeldiniz, " + name + " " + surname);
+
+        }
+    }
+}
