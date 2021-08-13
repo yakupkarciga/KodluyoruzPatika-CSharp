8a5759050db0a050bc936d4d207b53a1960c09f2
Batuhan Kıltaç <81926452+lbakteri@users.noreply.github.com>
2021-08-13 14:31:39 +0300

    Add files via upload

---
+++ switch-case/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace switch_case
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int month = DateTime.Now.Month;
+            // expression
+            switch (month)
+            {
+                case 1:
+                    Console.WriteLine("Ocak ayındasınız.");
+                    break;
+                case 2:
+                    Console.WriteLine("Şubat ayındasınız.");
+                    break;
+                case 3:
+                    Console.WriteLine("Mart ayındasınız.");
+                    break;
+                case 4:
+                    Console.WriteLine("Nisan ayındasınız.");
+                    break;
+                default:
+                    Console.WriteLine("Yanlış veri girişi!");
+                    break;
+                    // ****************************************************************
+            }
+
+            switch (month)
+            {
+                case 12:
+                case 1:
+                case 2:
+                    Console.WriteLine("Kış aylarındasınız.");
+                    break;
+                case 3:
+                case 4:
+                case 5:
+                    Console.WriteLine("İlkbahar aylarındasınız.");
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    Console.WriteLine("Yaz aylarındasınız.");
+                    break;
+                case 9:
+                case 10:
+                case 11:
+                    Console.WriteLine("Sonbahar aylarındasınız.");
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
