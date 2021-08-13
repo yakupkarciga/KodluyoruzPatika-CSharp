a16b30bf4aa5c47cc06753fdd70b9df9abb87222
Batuhan Kıltaç <81926452+lbakteri@users.noreply.github.com>
2021-08-13 18:22:43 +0300

    Add files via upload

---
+++ donguler/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace donguler
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Bir sayı giriniz: ");
+            int sayac = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= sayac; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            // ****************************************************************
+            int tekToplam = 0;
+            int ciftToplam = 0;
+            for (int i = 1; i <= 1000; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    tekToplam += i;
+                }
+                else
+                {
+                    ciftToplam += i;
+                }
+            }
+            Console.WriteLine("1000'e kadar olan tek sayılar toplamı: " + tekToplam);
+            Console.WriteLine("1000'e kadar olan çift sayılar toplamı: " + ciftToplam);
+
+            // ****************************************************************
+            for (int i = 1; i < 10; i++)
+            {
+                if (i == 4)
+                {
+                    break;
+                }
+                Console.WriteLine(i);
+            }
+
+            for (int i = 1; i < 10; i++)
+            {
+                if (i == 4)
+                {
+                    continue;
+                }
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
