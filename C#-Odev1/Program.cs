64722d849dcec0e376af044092add14e7f47bcaa
Batuhan Kıltaç <81926452+lbakteri@users.noreply.github.com>
2021-08-16 18:08:47 +0300

    Add files via upload

---
+++ C#-Odev1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace C__Odev1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kaç adet sayı girmek istersiniz ?: ");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Sayıları giriniz: ");
+            int numbers = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
