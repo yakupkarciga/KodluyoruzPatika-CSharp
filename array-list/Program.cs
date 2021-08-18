2c1320613cbcdb107d1dccfd29a204fa7392d166
Batuhan Kıltaç <81926452+lbakteri@users.noreply.github.com>
2021-08-18 15:20:46 +0300

    Add files via upload

---
+++ array-list/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace array_list
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            ArrayList liste = new ArrayList();
+            liste.Add("Batuhan");
+            liste.Add(2);
+            liste.Add(true);
+            liste.Add('B');
+
+
+            // verilere erişim
+            Console.WriteLine(liste[1]);
+            foreach (var i in liste)
+            {
+                Console.WriteLine(i);
+            }
+
+
+            // addrange
+            string[] renkler = { "kırmızı", "sarı", "yeşil" };
+            liste.AddRange(renkler);
+            List<int> sayilar = new List<int>() { 1, 8, 3, 7, 9, 92, 5 };
+            liste.AddRange(sayilar);
+            foreach (var i in liste)
+            {
+                Console.WriteLine(i);
+            }
+
+
+            // sort
+            liste.Sort();
+            foreach (var i in liste)
+            {
+                Console.WriteLine(i);
+            }
+
+
+            // binary search
+            Console.WriteLine(liste.BinarySearch(9));
+
+
+            // reverse
+            liste.Reverse();
+            foreach (var i in liste)
+            {
+                Console.WriteLine(i);
+            }
+
+
+            // clear
+            liste.Clear();
+            foreach (var i in liste)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
