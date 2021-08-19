18d2c72ed00d654bcd7111a66d2e6a50bd693534
Batuhan Kıltaç <81926452+lbakteri@users.noreply.github.com>
2021-08-19 17:59:10 +0300

    Add files via upload

---
+++ C#-ZOR SEVİYE PROJELER/Voting/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Voting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("C#-Voting");
+        }
+    }
+}
