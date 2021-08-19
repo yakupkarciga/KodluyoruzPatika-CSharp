7164f9b1bab7d7b935249462f846b766fe1d1770
Batuhan Kıltaç <81926452+lbakteri@users.noreply.github.com>
2021-08-19 14:36:37 +0300

    Add files via upload

---
+++ inheritance/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TohumluBitkiler tohumluBitki = new TohumluBitkiler();
+            tohumluBitki.TohumlaCogalma();
+
+            Console.WriteLine("------------------------");
+
+            Kuslar marti = new Kuslar();
+            marti.Ucmak();
+        }
+    }
+}
