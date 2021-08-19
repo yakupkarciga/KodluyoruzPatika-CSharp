755713d32268ecc331abe3a67d5092dedf764954
Batuhan Kıltaç <81926452+lbakteri@users.noreply.github.com>
2021-08-19 16:51:25 +0300

    Add files via upload

---
+++ C# PROJE-1 Console Telefon Rehberi Uygulaması/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace C__PROJE_1_Console_Telefon_Rehberi_Uygulaması
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // C#Proje1 - Console Telefon Rehberi Uygulaması (Daha sonra tamamlanacaktır.)
+            Console.WriteLine("C#Proje1 - Console Telefon Rehberi Uygulaması");
+        }
+    }
+}
