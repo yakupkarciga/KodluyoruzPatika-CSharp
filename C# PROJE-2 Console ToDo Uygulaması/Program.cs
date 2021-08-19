47e515b33b0649c1fe49d9431a682b017f4b2599
Batuhan Kıltaç <81926452+lbakteri@users.noreply.github.com>
2021-08-19 16:54:55 +0300

    Add files via upload

---
+++ C# PROJE-2 Console ToDo Uygulaması/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace C__PROJE_2_Console_ToDo_Uygulaması
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // C#Proje2 - Console ToDo Uygulaması (Daha sonra tamamlanacaktır.)
+            Console.WriteLine("C#Proje2 - Console ToDo Uygulaması");
+        }
+    }
+}
